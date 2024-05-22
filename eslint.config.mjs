368c335f03f37386af7000c7f9abda9fb6e7bdf0
Alexandre Perreau <alex.perreau@hotmail.fr>
2024-05-22 10:57:35 +0200

    Récupération des données pour la page d'acceuil

---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import globals from "globals";
+
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+    rules: {
+      semi: "error",
+      "quotes": ["error", "double"],
+      "prefer-const": "warn",
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    }
+  },
+];
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,47 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+        const article = document.createElement("article");
+
+        // const a = document.createElement("a");
+        // a.href = "./photographer.html";
+        // const img = document.createElement("img");
+        // img.setAttribute("src", picture);
+        // const h2 = document.createElement("h2");
+        // h2.textContent = name;
+        // a.appendChild(img);
+        // a.appendChild(h2);
+        
+        // const p = document.createElement("p");
+        // const locationDiv = document.createElement("div");
+        // locationDiv.textContent = `${city}, ${country}`;
+        // const taglineDiv = document.createElement("div");
+        // taglineDiv.textContent = tagline;
+        // const priceDiv = document.createElement("div");
+        // priceDiv.textContent = `${price}€/jour`;
+        // p.appendChild(locationDiv);
+        // p.appendChild(taglineDiv);
+        // p.appendChild(priceDiv);
+
+        // article.appendChild(a);
+        // article.appendChild(p);
+
+        const userCard = `
+            <a href="./photographer.html">
+                <img src=${picture}>
+                <h2>${name}</h2>
+            </a>
+            <div class="photographer_location">${city}, ${country}</div>
+            <div class="photographer_tagline">${tagline}</div>
+            <div class="photographer_price">${price}€/jour</div>
+        `;
+
+        article.innerHTML = userCard;
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }
